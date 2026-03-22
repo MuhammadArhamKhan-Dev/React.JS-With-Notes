9aa92b04aee0b2fa8f15cdc12d104764c3d06576
theLegend0000 <chemistryguider79@gmail.com>
2026-03-22 23:34:59 +0500

    09

---
+++ 09_ChildrenProp/App.js
@@ -0,0 +1,15 @@
+import AppleCounter from "./components/AppleCounter"
+import Counter from "./components/Counter"
+
+const App = () => {
+
+    return (
+        <>
+            <Counter>
+                <AppleCounter />
+            </Counter>
+        </>
+    )
+}
+
+export default App
---
+++ 09_ChildrenProp/components/Counter.js
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+const Counter = ({children}) => {
+
+  const [count, setCount] = useState(0)
+
+  // const myStateArr = useState(0)
+  // const count = myStateArr[0]
+  //const setCount = myStateArr[1]
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h1>{count}</h1>
+      <button onClick={() => {
+        // setCount(count + 1)
+        setCount(prev => prev + 1)
+        
+      }}>Count</button>
+      {children}
+    </>
+  )
+}
+
+export default Counter
